16c3893d1adc1c2b024cef647ea752fec62be3fe
rheael <rpandumpi@gmail.com>
2022-02-23 19:05:02 +0700

    ganti warna masih semua shape

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const state = new State();
 state.drawAll();
 
+//var r = JSON.stringify(state.shapeColor["red"]);
+//var g = JSON.stringify(state.shapeColor["green"]);
+//var b = JSON.stringify(state.shapeColor["blue"]);
+// shapeColor = [r, g, b];
+
 const drawNewShape = () => {
   state.addShape();
   state.drawAll();
@@ -17,6 +22,8 @@ canvas.addEventListener("click", e => {
   if(state.shape != Polygon && state.getCoordinatesLength() >= 2) {
     drawNewShape();
   }
+  //alert (JSON.stringify(state.shapeColor));
+  //alert(JSON.stringifiy(state.shapeList));
 });
 
 canvas.addEventListener("keypress", e => {
@@ -35,5 +42,37 @@ canvas.addEventListener("mousemove", e => {
       return;
     }
     (new state.shape(tempCoordinates, state.shapeColor)).draw(); 
+  }
+});
+
+
+// Baru buat warna
+canvas.addEventListener("mouseup", e => {
+  var color1 = parseInt(document.getElementById('color-picker').value.substr(1, 6),16,)
+  color1 = [Math.floor(color1 / 65536) / 255,
+  Math.floor((color1 % 65536) / 256) / 255,
+  (color1 % 256) / 255,]
+  // alert(color1)
+  var selectedVertex = -1
+  const {x, y} = state.getCursorCoordinate(e);
+  // alert(JSON.stringify(state.shapeList));
+  // Buat ubah warna, pencet salah satu titik sudut yg mau diubah
+    // Titik sudut dipencet = ubah warnanya sesuai color picker
+  alert(state.shapeList.length);
+  // Cari titik sudut yang dipencet kursor
+  // Tapi entah kenapa 1 warna berubah, berubah semua (?) sama berubah warna buat ngegambarnya
+  for(var i=0; i < state.shapeList.length;i++) {
+    //alert());
+    for(var j=0; j < state.shapeList[i].points.length ; j++) {
+      if (state.shapeList[i].points[j].x==x && state.shapeList[i].points[j].y==y) {
+        //alert(state.shapeList[i].points[j].x);
+        state.shapeList[i].color.red = color1[0];
+        state.shapeList[i].color.green = color1[1];
+        state.shapeList[i].color.blue = color1[2];    
+        //selectedVertex = i;
+        //alert(selectedVertex);
+        break;
+      }
+    }
   }
 });
